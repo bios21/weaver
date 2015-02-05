4a20ca611c3dca676a916e0e4b87ca74884fe765
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2015-02-05 16:55:40 +0100

    Structure tweek for client side support

--- weaver.js
+++ weaver.js
@@ -1,10 +1,98 @@
-/**
- * Create a new instance of weaver.
- *
- * @constructor
- * @this {Weaver}
- */
-exports.Weaver = function () {
+;(function (root, factory) {
+    if (exports) {
+        exports.Weaver = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define('Weaver', [], factory);
+    } else {
+        // Browser globals
+        root.Weaver = factory();
+    }
+}(this,
+
+function () {
+    /**
+     * Create a new instance of weaver.
+     *
+     * @constructor
+     * @this {Weaver}
+     */
+    var Weaver = function () {
+
+        /**
+         * Weave an aspect function after a list of joint points in the object
+         * @param {object} : Object in which weave
+         * @param {Array} : Array of jointPoints string (function names)
+         * @param {function} : Aspect function to weave after joint point functions
+         */
+        this.after = function(object, jointPoints, aspect) {
+            if (jointPoints instanceof Array && typeof aspect == 'function') {
+                for (var i = 0; i < jointPoints.length; i++) {
+                    var jointPoint = jointPoints[i];
+
+                    if (typeof jointPoint == 'string') {
+                        var oldFunction = find(object, jointPoint);
+                        if (oldFunction != null) {
+                            var replaceFunc = function () {
+                                oldFunction.apply(this, arguments);
+                                aspect.apply(this, arguments);
+                            };
+
+                            object[jointPoint] = replaceFunc;
+
+                        } else {
+                            throw new Error('Unknow function : ' + jointPoint);
+                        }
+                    } else {
+                        throw new Error('jointPoint at index ' + i + ' is not a String => typeof=' + typeof jointPoint);
+                    }
+                }
+            } else {
+                var errorMsg =
+                    (jointPoints instanceof Array)
+                    ? 'JointPoints must be an array of string'
+                    : 'Aspect must be a function';
+                throw new Error(errorMsg);
+            }
+        };
+
+        /**
+         * Weave an aspect function before a list of joint points in the object
+         * @param {object} : Object in which weave
+         * @param {Array} : Array of jointPoints string (function names)
+         * @param {function} : Aspect function to weave before joint point functions
+         */
+        this.before = function(object, jointPoints, aspect) {
+            if (jointPoints instanceof Array && typeof aspect == 'function') {
+                for (var i = 0; i < jointPoints.length; i++) {
+                    var jointPoint = jointPoints[i];
+
+                    if (typeof jointPoint == 'string') {
+                        var oldFunction = find(object, jointPoint);
+                        if (oldFunction != null) {
+                            var replaceFunc = function () {
+                                aspect.apply(this, arguments);
+                                oldFunction.apply(this, arguments);
+                            };
+
+                            object[jointPoint] = replaceFunc;
+
+                        } else {
+                            throw new Error('Unknow function : ' + jointPoint);
+                        }
+                    } else {
+                        throw new Error('jointPoint at index ' + i + ' is not a String => typeof=' + typeof jointPoint);
+                    }
+                }
+            } else {
+                var errorMsg =
+                    (jointPoints instanceof Array)
+                    ? 'JointPoints must be an array of string'
+                    : 'Aspect must be a function';
+                throw new Error(errorMsg);
+            }
+        };
+    };
 
     /**
      * Find a function in an object by name
@@ -22,77 +110,5 @@ exports.Weaver = function () {
         return objectFunction;
     };
 
-    /**
-     * Weave an aspect function after a list of joint points in the object
-     * @param {object} : Object in which weave
-     * @param {Array} : Array of jointPoints string (function names)
-     * @param {function} : Aspect function to weave after joint point functions
-     */
-    this.after = function(object, jointPoints, aspect) {
-        if (jointPoints instanceof Array && typeof aspect == 'function') {
-            for (var i = 0; i < jointPoints.length; i++) {
-                var jointPoint = jointPoints[i];
-                
-                if (typeof jointPoint == 'string') {
-                    var oldFunction = find(object, jointPoint);
-                    if (oldFunction != null) {
-                        var replaceFunc = function () {
-                            oldFunction.apply(this, arguments);
-                            aspect.apply(this, arguments);
-                        };
-
-                        object[jointPoint] = replaceFunc;
-
-                    } else {
-                        throw new Error('Unknow function : ' + jointPoint);
-                    }
-                } else {
-                    throw new Error('jointPoint at index ' + i + ' is not a String => typeof=' + typeof jointPoint);
-                }
-            }
-        } else {
-            var errorMsg =
-                (jointPoints instanceof Array)
-                ? 'JointPoints must be an array of string'
-                : 'Aspect must be a function';
-            throw new Error(errorMsg);
-        }
-    };
-
-    /**
-     * Weave an aspect function before a list of joint points in the object
-     * @param {object} : Object in which weave
-     * @param {Array} : Array of jointPoints string (function names)
-     * @param {function} : Aspect function to weave before joint point functions
-     */
-    this.before = function(object, jointPoints, aspect) {
-        if (jointPoints instanceof Array && typeof aspect == 'function') {
-            for (var i = 0; i < jointPoints.length; i++) {
-                var jointPoint = jointPoints[i];
-
-                if (typeof jointPoint == 'string') {
-                    var oldFunction = find(object, jointPoint);
-                    if (oldFunction != null) {
-                        var replaceFunc = function () {
-                            aspect.apply(this, arguments);
-                            oldFunction.apply(this, arguments);
-                        };
-
-                        object[jointPoint] = replaceFunc;
-
-                    } else {
-                        throw new Error('Unknow function : ' + jointPoint);
-                    }
-                } else {
-                    throw new Error('jointPoint at index ' + i + ' is not a String => typeof=' + typeof jointPoint);
-                }
-            }
-        } else {
-            var errorMsg =
-                (jointPoints instanceof Array)
-                ? 'JointPoints must be an array of string'
-                : 'Aspect must be a function';
-            throw new Error(errorMsg);
-        }
-    };
-};
+    return Weaver;
+}));
